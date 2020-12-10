32f6bfe04d2775fac3fdc0dabaafc79dc6046b31
francisudeji <francis.udejiofficial@gmail.com>
2020-12-10 14:10:03 +0100

    Add watcher to initial values

--- src/composables/useForm.js
+++ src/composables/useForm.js
@@ -1,7 +1,17 @@
-import { reactive } from 'vue';
+import { reactive, watchEffect, watch } from 'vue';
 
-function useForm({ initialValues, onSubmit }) {
-  const values = reactive(initialValues);
+function useForm({ defaultValues, onSubmit, defaultValidation }) {
+  const values = reactive(defaultValues);
+  console.log(defaultValidation, 'default');
+
+  watchEffect(() => console.log(values));
+
+  watch(
+    () => values.firstname,
+    (count) => {
+      console.log(count);
+    }
+  );
 
   function handleSubmit() {
     onSubmit(values);
